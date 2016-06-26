4638361330ad9f612543667369b8ef607f3e114d
arslan1999 <arslanlev1999@mail.ru>
2016-06-26 12:21:37 +0000

    Комментарий

---
+++ about.php
@@ -0,0 +1,48 @@
+<?php
+echo PHP_EOL;
+$title = 'Страница Хисамутдинова Арслана';
+$name = 'Арслан';
+$lastName = 'Хисамутдинов';
+$about = 'Фронтенд разработчик';
+$mail = 'arslanlev1999@mail.ru';
+$city = 'Ташкент';
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?= $title ?></title>
+        <style type="text/css">
+            div{background-color:#03b5fd; 
+            padding:10px; 
+            width:600px; 
+            height:auto;}
+            dl {display: table-row;}
+            dt, dd {display: table-cell;padding: 5px 10px;}
+        </style>
+    </head>
+    <body>
+        <div align="center">
+            <h1><?= $title ?></h1>
+            <dl>
+                <dt>Имя</dt>
+                <dd><?= $name ?></dd>
+            </dl>
+            <dl>
+                <dt>Фамилия</dt>
+                <dd><?= $lastName ?></dd>
+            </dl>
+            <dl>
+                <dt>О себе</dt>
+                <dd><?= $about ?></dd>
+            </dl>
+            <dl>
+                <dt>Почта</dt>
+                <dd><?= $mail ?></dd>
+            </dl>
+            <dl>
+                <dt>Город</dt>
+                <dd><?= $city ?></dd>
+            </dl>            
+        </div>
+    </body>
+</html>
