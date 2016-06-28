c6f6fb02ebfd638cbc15f0d877349c07f9c11009
arslan1999 <arslanlev1999@mail.ru>
2016-06-28 07:02:39 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+
+function get_title($title, $for_h1 = false) // Функция вывода h1 и title заголовков, переменная for_h1 уже встроенна
+{ 
+if ($for_h1 === false)
+return "<title>".$title."</title>";
+return "<h1>".$title."</h1>";
+}
+$title = "Угадай число заданное компьютером от 1 до 10";
+
+function game($a, $b){ //Алгоритм игры
+     if ( count($_GET) > 0 ){ // Проверяем получено ли значение
+	     if ($b > $a){ // Условие проверяющее введенное значение пользователя, больше задуманного числа или нет
+		     echo '<p>Много</p><p>Попробуй еще разочек</p>'; // если да то выводим сообщение "Много" и Просим повторить попытку
+	     }
+	     else if($b > false){ // Проверка введеного значения на нулевое значение
+		     if($b < $a){ // Проверяем значение пользователя, меньше задуманного числа или нет
+			     echo '<p>Мало</p><p>Попробуй еще разочек</p>'; //если да то выводим сообщение "Мало"  и Просим повторить попытку
+		     }
+		     else{ // Если же значение пользователя равно загаданному числу то выводим следующее сообщение
+			     echo '<p>Вы угадали</p>';
+		     }
+	     }
+		 else{ //Если введенное значение не больше заданного значения или же задано нулевое значение то
+			 echo '<p>Пожалуйста введите число от 1 до 10</p>';
+		 }
+     }
+}
+?>
+<html>
+     <head>
+         <?= get_title($title) ?> <!-- Выводим <title> заголовок -->
+         <meta http-equiv="Content-Type" content="text/html; charset=utf-8"></meta>
+         <style type="text/css">div{background-color:#03b5fd; padding:10px; width:600px; height:auto;margin:0px auto;text-align:center;}</style>
+     </head>
+	 
+     <body>
+         <div>
+             <?= get_title($title, true) ?> <!-- Выводим h1 заголовок -->
+             <form method="GET" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                 <input type="text" name="b" /><br /><br />
+                 <input type="submit" value="Испытать удачу" />
+				 <?= game(rand(1, 10), $_GET['b'])?>
+             </form>
+         </div>
+
+     </body>
+</html>
